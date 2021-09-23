05f7b41b27b78460938eba5694ef0a8ab15ee75e
jiro4989 <jiroron666@gmail.com>
2021-09-23 20:52:00 +0900

    apply prettier

--- src/installer.ts
+++ src/installer.ts
@@ -1,57 +1,57 @@
-import * as core from "@actions/core";
-import * as fs from "fs";
-import * as process from "process";
-import * as path from "path";
-import * as proc from "child_process";
-import * as util from "./util";
-const request = require("request-promise");
+import * as core from '@actions/core'
+import * as fs from 'fs'
+import * as process from 'process'
+import * as path from 'path'
+import * as proc from 'child_process'
+import * as util from './util'
+const request = require('request-promise')
 
 export async function getNim(version: string, noColor: boolean, yes: boolean) {
-  setNimbleBinPath();
-  await installNim(version, noColor, yes);
+  setNimbleBinPath()
+  await installNim(version, noColor, yes)
 }
 
 function setNimbleBinPath() {
-  let home = "";
-  if (process.platform === "win32") {
-    home = process.env["USERPROFILE"] || "";
+  let home = ''
+  if (process.platform === 'win32') {
+    home = process.env['USERPROFILE'] || ''
   } else {
-    home = process.env["HOME"] || "";
+    home = process.env['HOME'] || ''
   }
-  const binPath = path.join(home, ".nimble", "bin");
-  const p = process.env["PATH"] || "";
-  let newPath = "";
-  if (process.platform === "win32") {
-    newPath = `${binPath};${p}`;
+  const binPath = path.join(home, '.nimble', 'bin')
+  const p = process.env['PATH'] || ''
+  let newPath = ''
+  if (process.platform === 'win32') {
+    newPath = `${binPath};${p}`
   } else {
-    newPath = `${binPath}:${p}`;
+    newPath = `${binPath}:${p}`
   }
-  core.exportVariable("PATH", newPath);
+  core.exportVariable('PATH', newPath)
 }
 
 async function installNim(version: string, noColor: boolean, yes: boolean) {
   const body = await request({
-    url: "https://nim-lang.org/choosenim/init.sh",
-    method: "GET",
-  });
-  fs.writeFileSync("init.sh", body);
-  process.env.CHOOSENIM_NO_ANALYTICS = "1";
+    url: 'https://nim-lang.org/choosenim/init.sh',
+    method: 'GET',
+  })
+  fs.writeFileSync('init.sh', body)
+  process.env.CHOOSENIM_NO_ANALYTICS = '1'
 
   // #38
   if (util.isGlobPatchVersion(version) || util.isGlobMinorVersion(version)) {
-    core.info(`Fetch a latest versions with ${version}`);
-    version = await util.getLatestVersion(version);
+    core.info(`Fetch a latest versions with ${version}`)
+    version = await util.getLatestVersion(version)
   }
 
   // #21
-  if (process.platform === "win32") {
-    process.env.CHOOSENIM_CHOOSE_VERSION = version;
-    proc.exec("sh init.sh -y", (err: any, stdout: string, stderr: string) => {
+  if (process.platform === 'win32') {
+    process.env.CHOOSENIM_CHOOSE_VERSION = version
+    proc.exec('sh init.sh -y', (err: any, stdout: string, stderr: string) => {
       if (err) {
-        core.error(err);
-        throw err;
+        core.error(err)
+        throw err
       }
-      core.info(stdout);
+      core.info(stdout)
 
       // #41
       // WindowsのみなぜかZIPファイルが展開されないので一度別バージョンにスイッチ
@@ -60,51 +60,51 @@ async function installNim(version: string, noColor: boolean, yes: boolean) {
         `choosenim.exe 1.4.0`,
         (err: any, stdout: string, stderr: string) => {
           if (err) {
-            core.error(err);
-            throw err;
+            core.error(err)
+            throw err
           }
-          core.info(stdout);
+          core.info(stdout)
           proc.exec(
             `choosenim.exe ${version}`,
             (err: any, stdout: string, stderr: string) => {
               if (err) {
-                core.error(err);
-                throw err;
+                core.error(err)
+                throw err
               }
-              core.info(stdout);
+              core.info(stdout)
             }
-          );
+          )
         }
-      );
-    });
-    return;
+      )
+    })
+    return
   }
 
-  proc.exec("sh init.sh -y", (err: any, stdout: string, stderr: string) => {
+  proc.exec('sh init.sh -y', (err: any, stdout: string, stderr: string) => {
     if (err) {
-      core.error(err);
-      throw err;
+      core.error(err)
+      throw err
     }
-    core.info(stdout);
+    core.info(stdout)
 
     // Build optional parameters of choosenim.
-    let opts: string[] = [];
-    if (noColor) opts.push("--noColor");
-    if (yes) opts.push("--yes");
-    let optsStr = "";
+    let opts: string[] = []
+    if (noColor) opts.push('--noColor')
+    if (yes) opts.push('--yes')
+    let optsStr = ''
     if (0 < opts.length) {
-      optsStr = " " + opts.join(" ");
+      optsStr = ' ' + opts.join(' ')
     }
 
     proc.exec(
       `choosenim ${version}${optsStr}`,
       (err: any, stdout: string, stderr: string) => {
         if (err) {
-          core.error(err);
-          throw err;
+          core.error(err)
+          throw err
         }
-        core.info(stdout);
+        core.info(stdout)
       }
-    );
-  });
+    )
+  })
 }
--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,178 +1,178 @@
-import * as util from "../src/util";
+import * as util from '../src/util'
 
-describe("function isGlobPatchVersion", (): void => {
-  test("ok: 1.2.x", (): void => {
-    expect(util.isGlobPatchVersion("1.2.x")).toBe(true);
-  });
+describe('function isGlobPatchVersion', (): void => {
+  test('ok: 1.2.x', (): void => {
+    expect(util.isGlobPatchVersion('1.2.x')).toBe(true)
+  })
 
-  test("ok: 1.99.x", (): void => {
-    expect(util.isGlobPatchVersion("1.99.x")).toBe(true);
-  });
+  test('ok: 1.99.x', (): void => {
+    expect(util.isGlobPatchVersion('1.99.x')).toBe(true)
+  })
 
-  test("ng: 1.2.3", (): void => {
-    expect(util.isGlobPatchVersion("1.2.3")).toBe(false);
-  });
+  test('ng: 1.2.3', (): void => {
+    expect(util.isGlobPatchVersion('1.2.3')).toBe(false)
+  })
 
-  test("ng: 1.x", (): void => {
-    expect(util.isGlobPatchVersion("1.x")).toBe(false);
-  });
+  test('ng: 1.x', (): void => {
+    expect(util.isGlobPatchVersion('1.x')).toBe(false)
+  })
 
-  test("ng: 寿司", (): void => {
-    expect(util.isGlobPatchVersion("寿司")).toBe(false);
-  });
-});
+  test('ng: 寿司', (): void => {
+    expect(util.isGlobPatchVersion('寿司')).toBe(false)
+  })
+})
 
-describe("function isGlobMinorVersion", (): void => {
-  test("ok: 1.x", (): void => {
-    expect(util.isGlobMinorVersion("1.x")).toBe(true);
-  });
+describe('function isGlobMinorVersion', (): void => {
+  test('ok: 1.x', (): void => {
+    expect(util.isGlobMinorVersion('1.x')).toBe(true)
+  })
 
-  test("ok: 99.x", (): void => {
-    expect(util.isGlobMinorVersion("99.x")).toBe(true);
-  });
+  test('ok: 99.x', (): void => {
+    expect(util.isGlobMinorVersion('99.x')).toBe(true)
+  })
 
-  test("ng: 1.2", (): void => {
-    expect(util.isGlobMinorVersion("1.2")).toBe(false);
-  });
+  test('ng: 1.2', (): void => {
+    expect(util.isGlobMinorVersion('1.2')).toBe(false)
+  })
 
-  test("ng: 寿司", (): void => {
-    expect(util.isGlobMinorVersion("寿司")).toBe(false);
-  });
-});
+  test('ng: 寿司', (): void => {
+    expect(util.isGlobMinorVersion('寿司')).toBe(false)
+  })
+})
 
-describe("function fetchTagList", (): void => {
-  test("returns some array objects", (): void => {
+describe('function fetchTagList', (): void => {
+  test('returns some array objects', (): void => {
     util.fetchTagList().then((tags: any[]) => {
-      expect(0 < tags.length).toBe(true);
-    });
-  });
-});
+      expect(0 < tags.length).toBe(true)
+    })
+  })
+})
 
-describe("function getLatestVersionWithTags", (): void => {
-  test("ok: returns a matched latest patch version from 5 versions", (): void => {
-    const want = "1.2.3";
-    const got = util.getLatestVersionWithTags("1.2.x", [
-      "v1.0.3",
-      "v1.2.1",
-      "v1.2.2",
-      "v1.2.3",
-      "v1.20.3",
-    ]);
-    expect(got).toBe(want);
-  });
+describe('function getLatestVersionWithTags', (): void => {
+  test('ok: returns a matched latest patch version from 5 versions', (): void => {
+    const want = '1.2.3'
+    const got = util.getLatestVersionWithTags('1.2.x', [
+      'v1.0.3',
+      'v1.2.1',
+      'v1.2.2',
+      'v1.2.3',
+      'v1.20.3',
+    ])
+    expect(got).toBe(want)
+  })
 
-  test("ok: returns a matched latest patch version from 1 version", (): void => {
-    const want = "1.2.3";
-    const got = util.getLatestVersionWithTags("1.2.x", ["v1.2.3"]);
-    expect(got).toBe(want);
-  });
+  test('ok: returns a matched latest patch version from 1 version', (): void => {
+    const want = '1.2.3'
+    const got = util.getLatestVersionWithTags('1.2.x', ['v1.2.3'])
+    expect(got).toBe(want)
+  })
 
-  test("ok: returns a matched latest minor version from 5 versions", (): void => {
-    const want = "1.10.0";
-    const got = util.getLatestVersionWithTags("1.x", [
-      "v1.1.1",
-      "v1.2.2",
-      "v1.3.3",
-      "v1.4.4-beta",
-      "v1.10.0",
-    ]);
-    expect(got).toBe(want);
-  });
+  test('ok: returns a matched latest minor version from 5 versions', (): void => {
+    const want = '1.10.0'
+    const got = util.getLatestVersionWithTags('1.x', [
+      'v1.1.1',
+      'v1.2.2',
+      'v1.3.3',
+      'v1.4.4-beta',
+      'v1.10.0',
+    ])
+    expect(got).toBe(want)
+  })
 
-  test("ok: returns a matched latest minor version from 1 version", (): void => {
-    const want = "1.9.0";
-    const got = util.getLatestVersionWithTags("1.x", ["v1.9.0"]);
-    expect(got).toBe(want);
-  });
+  test('ok: returns a matched latest minor version from 1 version', (): void => {
+    const want = '1.9.0'
+    const got = util.getLatestVersionWithTags('1.x', ['v1.9.0'])
+    expect(got).toBe(want)
+  })
 
-  test("ok: returns a matched latest special patch version (-beta)", (): void => {
-    const want = "1.2.5-beta";
-    const got = util.getLatestVersionWithTags("1.2.x", [
-      "v1.2.1",
-      "v1.2.2",
-      "v1.2.3-rc1",
-      "v1.2.4",
-      "v1.2.5-beta",
-    ]);
-    expect(got).toBe(want);
-  });
+  test('ok: returns a matched latest special patch version (-beta)', (): void => {
+    const want = '1.2.5-beta'
+    const got = util.getLatestVersionWithTags('1.2.x', [
+      'v1.2.1',
+      'v1.2.2',
+      'v1.2.3-rc1',
+      'v1.2.4',
+      'v1.2.5-beta',
+    ])
+    expect(got).toBe(want)
+  })
 
   test("ok: returns a empty string when a version doesn't match", (): void => {
-    const want = "";
-    const got = util.getLatestVersionWithTags("1.2.x", [
-      "v1.0.0",
-      "v1.1.0",
-      "v1.3.0",
-      "v1.3.x",
-    ]);
-    expect(got).toBe(want);
-  });
+    const want = ''
+    const got = util.getLatestVersionWithTags('1.2.x', [
+      'v1.0.0',
+      'v1.1.0',
+      'v1.3.0',
+      'v1.3.x',
+    ])
+    expect(got).toBe(want)
+  })
 
   test("ok: returns a empty string when a version doesn't match (minor)", (): void => {
-    const want = "";
-    const got = util.getLatestVersionWithTags("1.x", [
-      "v1.a",
-      "vtuber",
-      "v",
-      "vvvv",
-      "v1",
-      "寿司",
-    ]);
-    expect(got).toBe(want);
-  });
+    const want = ''
+    const got = util.getLatestVersionWithTags('1.x', [
+      'v1.a',
+      'vtuber',
+      'v',
+      'vvvv',
+      'v1',
+      '寿司',
+    ])
+    expect(got).toBe(want)
+  })
 
   test("ok: returns a empty string when a version doesn't match (patch)", (): void => {
-    const want = "";
-    const got = util.getLatestVersionWithTags("1.2.x", [
-      "v1.a",
-      "vtuber",
-      "v",
-      "vvvv",
-      "v1",
-    ]);
-    expect(got).toBe(want);
-  });
+    const want = ''
+    const got = util.getLatestVersionWithTags('1.2.x', [
+      'v1.a',
+      'vtuber',
+      'v',
+      'vvvv',
+      'v1',
+    ])
+    expect(got).toBe(want)
+  })
 
-  test("ng: returns a empty string when the first argument is illagal", (): void => {
-    const want = "";
-    const got = util.getLatestVersionWithTags("1.2.9", [
-      "v1.2.1",
-      "v1.2.2",
-      "v1.2.3",
-    ]);
-    expect(got).toBe(want);
-  });
+  test('ng: returns a empty string when the first argument is illagal', (): void => {
+    const want = ''
+    const got = util.getLatestVersionWithTags('1.2.9', [
+      'v1.2.1',
+      'v1.2.2',
+      'v1.2.3',
+    ])
+    expect(got).toBe(want)
+  })
 
-  test("ng: returns a empty string when the first argument is illagal", (): void => {
-    const want = "";
-    const got = util.getLatestVersionWithTags("寿司", [
-      "v1.2.1",
-      "v1.2.2",
-      "v1.2.3",
-    ]);
-    expect(got).toBe(want);
-  });
+  test('ng: returns a empty string when the first argument is illagal', (): void => {
+    const want = ''
+    const got = util.getLatestVersionWithTags('寿司', [
+      'v1.2.1',
+      'v1.2.2',
+      'v1.2.3',
+    ])
+    expect(got).toBe(want)
+  })
 
-  test("ng: returns a empty string when the first argument is a empty string", (): void => {
-    const want = "";
-    const got = util.getLatestVersionWithTags("", [
-      "v1.2.1",
-      "v1.2.2",
-      "v1.2.3",
-    ]);
-    expect(got).toBe(want);
-  });
+  test('ng: returns a empty string when the first argument is a empty string', (): void => {
+    const want = ''
+    const got = util.getLatestVersionWithTags('', [
+      'v1.2.1',
+      'v1.2.2',
+      'v1.2.3',
+    ])
+    expect(got).toBe(want)
+  })
 
-  test("ng: returns a version of the first argument when the second argument length is 0", (): void => {
-    const want = "1.2.x";
-    const got = util.getLatestVersionWithTags("1.2.x", []);
-    expect(got).toBe(want);
-  });
+  test('ng: returns a version of the first argument when the second argument length is 0', (): void => {
+    const want = '1.2.x'
+    const got = util.getLatestVersionWithTags('1.2.x', [])
+    expect(got).toBe(want)
+  })
 
-  test("ng: returns a version of the first argument when the second argument is null", (): void => {
-    const want = "1.2.x";
-    const tags: any = null;
-    const got = util.getLatestVersionWithTags("1.2.x", tags);
-    expect(got).toBe(want);
-  });
-});
+  test('ng: returns a version of the first argument when the second argument is null', (): void => {
+    const want = '1.2.x'
+    const tags: any = null
+    const got = util.getLatestVersionWithTags('1.2.x', tags)
+    expect(got).toBe(want)
+  })
+})
