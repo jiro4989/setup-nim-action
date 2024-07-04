bcf7dbf64ff8eddb85ab6c1f5a508f7bc588cbfe
jiro4989 <jiroron666@gmail.com>
2024-07-05 08:57:22 +0900

    latest install

--- install_nim.sh
+++ install_nim.sh
@@ -2,8 +2,16 @@
 
 set -eu
 
+fetch_tags() {
+  # https://docs.github.com/ja/rest/git/refs?apiVersion=2022-11-28
+  curl \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/nim-lang/nim/git/refs/tags
+}
+
 output_dir=".nim_runtime"
-nim_version="2.0.8"
+# nim_version="2.0.8"
+nim_version="$(fetch_tags | jq -r '.[].ref' | sort -V | tail -n 1 | sed -E 's:^refs/tags/v::')"
 os="linux"
 arch="x64"
 download_url="https://nim-lang.org/download/nim-${nim_version}-${os}_${arch}.tar.xz"
