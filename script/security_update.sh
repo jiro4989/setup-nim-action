004cfeadee136c9c2ebae7488d36bdbdbaaf7ef0
jiro <jiroron666@gmail.com>
2024-07-06 11:04:14 +0900

    chore: delete unused files for typescript #492 (#493)
    * del: unused files
    
    * del: dependabot for npm

--- script/security_update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-today=$(date +%Y-%m-%d)
-git checkout -b "chore/security-update-$today"
-npm audit fix
-git add package*.json node_modules
-git commit -m ':shield: security update'
